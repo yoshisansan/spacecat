1408e2471bd9deb4bae3ea38c2dcccfdebc59980
Akifumi <kusacurrency@gmail.com>
2019-02-14 08:11:19 +0900

    delete home

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Header.css';
+import Styled from 'styled-components';
+
+export default class Header extends Component {
+
+  render() {
+    return(
+       <Header></Header>
+    )
+  }
+}
+
+const Header = styled.h1`
+    font-size: 20rem;
+`
